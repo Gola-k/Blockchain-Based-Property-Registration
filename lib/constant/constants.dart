3f06aab853d3f978ff49c17df3a7d5e665b7acee
Gola-k <kunalgola426@gmail.com>
2023-10-15 17:22:44 +0530

    1st commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,12 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDg0QTYzNTVhRDg1QzQ4ODVDRDE5NzZCZDA5QUEzQjQyNDVlQzZGNDYiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5MjUxOTY3ODQ3MSwibmFtZSI6InByb3BlcnR5cmVnaXN0cmF0aW9uIn0.H5kz1mJw7AAMQ53r9jn20q_kVNxr2vzACaBPMWnpe1Q';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+// "https://rpc-mumbai.maticvigil.com/v1 /a5be973518c173bacd9be16a6314dd08b6abcd23";
+
+const String contractAddress = "0xBF4b239C4dBF1D6d30Da2de91B7Fa115992C7c14";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
